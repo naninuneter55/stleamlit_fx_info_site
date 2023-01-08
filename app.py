f53ef87f77530899eee7ded1024a74c936f1dad0
Koji ONO <naninuneter55@gmail.com>
2023-01-08 12:45:20 +0900

    foo

--- app.py
+++ app.py
@@ -1,92 +1,45 @@
+from datetime import date
 import streamlit as st
-import plotly.graph_objs as go
-import requests
-import os
-import sys
-import subprocess
-import shutil
-import numpy
-import platform
 import pandas as pd
 import gspread
-# from oauth2client.service_account import ServiceAccountCredentials
 from google.oauth2 import service_account
 from gspread_dataframe import get_as_dataframe
-
-if platform.system() == "Linux":
-    # shutil.rmtree("/tmp/ta-lib")
-    # check if the library folder already exists, to avoid building everytime you load the pahe
-    if not os.path.isdir("/tmp/ta-lib"):
-
-        # Download ta-lib to disk
-        with open("/tmp/ta-lib-0.4.0-src.tar.gz", "wb") as file:
-            response = requests.get(
-                "http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz"
-            )
-            file.write(response.content)
-        # get our current dir, to configure it back again. Just house keeping
-        default_cwd = os.getcwd()
-        os.chdir("/tmp")
-        # untar
-        os.system("tar -zxvf ta-lib-0.4.0-src.tar.gz")
-        os.chdir("/tmp/ta-lib")
-        os.system("ls -la /app/equity/")
-        # build
-        os.system("./configure --prefix=/home/appuser")
-        os.system("make")
-        # install
-        os.system("make install")
-        # back to the cwd
-        os.chdir(default_cwd)
-        sys.stdout.flush()
-
-    # add the library to our current environment
-    from ctypes import *
-
-    lib = CDLL("/home/appuser/lib/libta_lib.so.0.0.0")
-    # import library
-    try:
-        import talib
-    except ImportError:
-        os.environ['CFLAGS'] = '-I/home/appuser/include/'
-        os.environ['LDFLAGS'] = '-L/home/appuser/lib/'
-        # subprocess.check_call([sys.executable, "-m", "pip", "install", "--global-option=build_ext", "--global-option=-L/home/appuser/lib/", "--global-option=-I/home/appuser/include/", "TA-Lib"])
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "TA-Lib"])
-    # finally:
-    #     import talib
-
-import talib as ta
+import mplfinance as mpf
 
 scope =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-# creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-creds = service_account.Credentials.from_service_account_info( st.secrets["gcp_service_account"], scopes=scope)
-
-gc = gspread.authorize(creds)
+creds = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
 SP_SHEET_KEY = st.secrets.SP_SHEET_KEY.key # スプレッドシートのキー
+gc = gspread.authorize(creds)
 sh = gc.open_by_key(SP_SHEET_KEY)
-SP_SHEET = 'シート1' # シート名「シート1」を指定
-worksheet = sh.worksheet(SP_SHEET)
-df = get_as_dataframe(worksheet)
-df["time"] = pd.to_datetime(df["time"])
-df = df.set_index("time")
-df.rename(columns={'tick_volume':'volume'}, inplace=True)
-
-close = df['close']
-macd, macdsignal, _ = ta.MACD(close, fastperiod=12, slowperiod=26, signalperiod=9)
-df['macd'] = macd
-df['macd_signal'] = macdsignal
-# data = worksheet.get_all_values() 
-# print(len(data))
 
 
+@st.cache
+def get_historical_data(from_dt, to_dt):
+    SP_SHEET = 'シート1' # シート名「シート1」を指定
+    worksheet = sh.worksheet(SP_SHEET)
+    df = get_as_dataframe(worksheet, header=0, index_col=0)
+    df = df.rename(columns = {'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Volume': 'volume'})
+    for i in df.columns:
+        df[i] = df[i].astype(float)
+    df.index = pd.to_datetime(df.index)
+    df = df.loc[from_dt:to_dt]
+    return df
+
+from_dt = date(2022, 1, 1)
+to_dt = date(2022, 3, 31)
+df = get_historical_data(from_dt, to_dt)
 st.title("FX Infomation")
+
+fig, ax = mpf.plot(
+    df,
+    title=f'foo',
+    type="candle",
+    show_nontrading=True,
+    volume=True,
+    style="yahoo",
+    figsize=(15,10),
+    returnfig=True
+)
+
+st.pyplot(fig)
 st.dataframe(df)
-
-mdf = df.tail(100)  # 直近100日分のデータ
-
-fig = go.Figure(data = [go.Candlestick(x = mdf.index,\
-    open = mdf['open'],\
-    high = mdf['high'],\
-    low = mdf['low'],\
-    close = mdf['close'])])
-st.plotly_chart(fig, theme="streamlit", use_container_width=True)
